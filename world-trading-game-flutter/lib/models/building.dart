bf20f2027254e2b5ffe2aab0a42a6eb444196824
Youssef Ramy <98766548+YoussefRamy-12@users.noreply.github.com>
2026-08-20 11:40:25 +0300

    refactor(flutter): remove building upgrade level model

--- world-trading-game-flutter/lib/models/building.dart
+++ world-trading-game-flutter/lib/models/building.dart
@@ -7,12 +7,24 @@ class BuildingType {
   final int maintenanceCost;
   final int maxLevel;
 
-  const BuildingType({required this.id, required this.slug, required this.name, this.baseCost = 0, this.baseIncome = 0, this.maintenanceCost = 0, this.maxLevel = 1});
+  const BuildingType({
+    required this.id,
+    required this.slug,
+    required this.name,
+    this.baseCost = 0,
+    this.baseIncome = 0,
+    this.maintenanceCost = 0,
+    this.maxLevel = 1,
+  });
 
   factory BuildingType.fromMap(Map<String, dynamic> map) => BuildingType(
-        id: _int(map['id']), slug: (map['slug'] ?? '').toString(), name: (map['name'] ?? '').toString(),
-        baseCost: _int(map['base_cost']), baseIncome: _int(map['base_income']),
-        maintenanceCost: _int(map['maintenance_cost']), maxLevel: _int(map['max_level'], 1),
+        id: _int(map['id']),
+        slug: (map['slug'] ?? '').toString(),
+        name: (map['name'] ?? '').toString(),
+        baseCost: _int(map['base_cost']),
+        baseIncome: _int(map['base_income']),
+        maintenanceCost: _int(map['maintenance_cost']),
+        maxLevel: _int(map['max_level'], 1),
       );
 
   static int _int(dynamic v, [int fallback = 0]) => v is num ? v.toInt() : int.tryParse('$v') ?? fallback;
@@ -22,14 +34,20 @@ class CountryBuilding {
   final String id;
   final String countryId;
   final int buildingTypeId;
-  final int level;
   final int count;
 
-  const CountryBuilding({required this.id, required this.countryId, required this.buildingTypeId, this.level = 1, this.count = 1});
+  const CountryBuilding({
+    required this.id,
+    required this.countryId,
+    required this.buildingTypeId,
+    this.count = 1,
+  });
 
   factory CountryBuilding.fromMap(Map<String, dynamic> map) => CountryBuilding(
-        id: map['id'].toString(), countryId: map['country_id'].toString(),
-        buildingTypeId: _int(map['building_type_id']), level: _int(map['level'], 1), count: _int(map['count'], 1),
+        id: map['id'].toString(),
+        countryId: map['country_id'].toString(),
+        buildingTypeId: _int(map['building_type_id']),
+        count: _int(map['count'], 1),
       );
 
   static int _int(dynamic v, [int fallback = 0]) => v is num ? v.toInt() : int.tryParse('$v') ?? fallback;
